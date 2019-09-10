bd94d0db1c09dc80df655f337efbcbc2b58b86b3
Rana Hossam <ranahossam263@gmail.com>
2019-09-10 19:11:45 +0200

    Tabs for child and teacher

---
+++ skolera/skolera/Home Page/Controllers/Nav Controllers/TeacherCoursesTableViewNVC.swift
@@ -0,0 +1,30 @@
+//
+//  TeacherCoursesTableViewNVC.swift
+//  skolera
+//
+//  Created by Rana Hossam on 9/10/19.
+//  Copyright © 2019 Skolera. All rights reserved.
+//
+
+import UIKit
+
+class TeacherCoursesTableViewNVC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
