2c63750f83306740b5f2e991446819252e1b6b56
Rana Hossam <ranahossam263@gmail.com>
2019-09-16 18:44:20 +0200

    Full day attendance ui fix
    reset cell state

--- skolera/skolera/Attendance/Controllers/Teacher/TeacherAttendanceViewController.swift
+++ skolera/skolera/Attendance/Controllers/Teacher/TeacherAttendanceViewController.swift
@@ -135,14 +135,14 @@ class TeacherAttendanceViewController: UIViewController {
 
 extension TeacherAttendanceViewController: UITableViewDelegate, UITableViewDataSource {
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.students.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var flag = false
+//        var flag = false
         let cell = tableView.dequeueReusableCell(withIdentifier: "teacherAttendanceCell") as! TeacherAttendanceTableViewCell
+        cell.resetAll()
         cell.studentSelectButton.layer.borderWidth = 1
         cell.studentSelectButton.layer.borderColor = #colorLiteral(red: 0.6470588235, green: 0.6784313725, blue: 0.7058823529, alpha: 1)
         cell.didSelectStudent = {
@@ -162,7 +162,6 @@ extension TeacherAttendanceViewController: UITableViewDelegate, UITableViewDataS
             switch state {
             case .present:
                 cell.presentSelected()
-                
             case .late:
                 cell.lateSelected()
             case .absent:
@@ -174,16 +173,15 @@ extension TeacherAttendanceViewController: UITableViewDelegate, UITableViewDataS
         }
         cell.studentNameLabel.text = self.students[indexPath.row].name ?? ""
         cell.studentImageView.childImageView(url: self.students[indexPath.row].avatarUrl, placeholder: "\(self.students[indexPath.row].name.prefix(2).uppercased())", textSize: 14)
-        flag = attendanceStudents.contains(where: { (attendanceStudent) -> Bool in
-            self.students[indexPath.row].childId == attendanceStudent.childId
-        })
-        
+//        flag = attendanceStudents.contains(where: { (attendanceStudent) -> Bool in
+//            self.students[indexPath.row].childId == attendanceStudent.childId
+//        })
         if let cellAttendance = self.attedances.first(where: { $0.student.childId == self.students[indexPath.row].childId}) {
-            debugPrint("<#T##items: Any...##Any#>")
             if let _ = cellAttendance.timetableSlotId {
-                flag = false
+//                flag = false
+                cell.resetAll()
             } else {
-                flag = true
+//                flag = true
                 //present the state for this student
                 switch cellAttendance.status!  {
                 case "present":
@@ -195,11 +193,10 @@ extension TeacherAttendanceViewController: UITableViewDelegate, UITableViewDataS
                 case "excused":
                     cell.excusedSelected()
                 default:
-                    debugPrint("<#T##items: Any...##Any#>")
+                    debugPrint("default case")
                 }
             }
         }
-
         return cell
     }
     
