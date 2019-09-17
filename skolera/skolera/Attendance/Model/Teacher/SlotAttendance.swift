0be555a5605c389504e7d3c426c440698774deba
Rana Hossam <ranahossam263@gmail.com>
2019-09-17 19:07:11 +0200

    Slots attendance new components
    Slots attendance arrays for response

---
+++ skolera/skolera/Attendance/Model/Teacher/SlotAttendance.swift
@@ -0,0 +1,9 @@
+//
+//  SlotAttendance.swift
+//  skolera
+//
+//  Created by Rana Hossam on 9/17/19.
+//  Copyright © 2019 Skolera. All rights reserved.
+//
+
+import Foundation
---
+++ skolera/skolera/Attendance/Model/Teacher/TimeTableSlots.swift
@@ -0,0 +1,9 @@
+//
+//  TimeTableSlots.swift
+//  skolera
+//
+//  Created by Rana Hossam on 9/17/19.
+//  Copyright © 2019 Skolera. All rights reserved.
+//
+
+import Foundation
