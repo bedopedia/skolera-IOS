d7978df98f72565310943c61d62fed1aa7ceec87
Rana Hossam <ranahossam263@gmail.com>
2019-09-30 13:21:48 +0200

    Match cell border

--- skolera/skolera/Quizzes/Views/QuizAnswerTableViewCell.swift
+++ skolera/skolera/Quizzes/Views/QuizAnswerTableViewCell.swift
@@ -19,7 +19,11 @@ class QuizAnswerTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        matchLeftView.layer.borderWidth = 1
+        matchLeftView.layer.borderColor = #colorLiteral(red: 0.6470588235, green: 0.6784313725, blue: 0.7058823529, alpha: 1)
+        matchLeftView.layer.cornerRadius = 6
+//        matchLeftView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
