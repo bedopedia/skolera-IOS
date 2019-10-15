28302d5e5c0d284768f856aa94c9568eb15f4ddf
Rana Hossam <ranahossam263@gmail.com>
2019-10-15 16:59:23 +0200

    Refactor Network layer in NewMessageViewController

---
+++ skolera/skolera/Threads/Network/ThreadsNetworkApiManager.swift
@@ -0,0 +1,26 @@
+//
+//  ThreadsNetworkApiManager.swift
+//  skolera
+//
+//  Created by Rana Hossam on 10/15/19.
+//  Copyright © 2019 Skolera. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+func getSubjectsApi(parameters: Parameters, child: Child, completion: @escaping ((Bool, Int, Any?, Error?) -> ())) {
+    let headers : HTTPHeaders? = getHeaders()
+    let url = String(format: GET_THREADS_COURSE_GROUPS(),child.actableId)
+    debugPrint(url)
+    Alamofire.request(url, method: .get, parameters: parameters, headers: headers).validate().responseJSON { response in
+        switch response.result{
+        case .success(_):
+            completion(true, response.response?.statusCode ?? 0, response.result.value, nil)
+        case .failure(let error):
+            completion(false, response.response?.statusCode ?? 0, nil, error)
+        }
+    }
+}
+
