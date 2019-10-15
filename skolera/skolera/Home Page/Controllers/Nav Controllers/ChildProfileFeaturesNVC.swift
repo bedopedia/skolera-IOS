898be910a31abb7c2a642561148869da826ca431
Rana Hossam <ranahossam263@gmail.com>
2019-10-15 11:36:14 +0200

    Turned on swipe gesture for ChildProfileFeaturesNVC

--- skolera/skolera/Home Page/Controllers/Nav Controllers/ChildProfileFeaturesNVC.swift
+++ skolera/skolera/Home Page/Controllers/Nav Controllers/ChildProfileFeaturesNVC.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ChildProfileFeaturesNVC: UINavigationController {
-
+class ChildProfileFeaturesNVC: UINavigationController, UIGestureRecognizerDelegate, UINavigationControllerDelegate {
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.isNavigationBarHidden = true
 //        self.interactivePopGestureRecognizer?.isEnabled = true
-//        self.interactivePopGestureRecognizer?.delegate = self as? UIGestureRecognizerDelegate
+        self.navigationController?.delegate = self
+        self.interactivePopGestureRecognizer?.delegate = self 
+    }
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        let enable = self.navigationController?.viewControllers.count ?? 0 > 1
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = enable
     }
     
-
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
     /*
     // MARK: - Navigation
 
