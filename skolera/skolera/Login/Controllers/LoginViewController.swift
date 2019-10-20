35bc9c7198b406536a5f0497281e426477f58751
Rana Hossam <ranahossam263@gmail.com>
2019-10-20 12:10:55 +0200

    Revert "Merge branch 'Fix-student-login' into origin/master"
    This reverts commit 07f8a7b5a0144778b83299a89ac293d851706d68.

--- skolera/skolera/Login/Controllers/LoginViewController.swift
+++ skolera/skolera/Login/Controllers/LoginViewController.swift
@@ -180,8 +180,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate, NVActivityIndi
                         if let _ = parent.data.parentId {
                             self.getChildren(parentId: parent.data.parentId, childId: parent.data.actableId)
                         } else {
-                            self.stopAnimating()
-                            showNetworkFailureError(viewController: self, statusCode: -1, error: NSError())
+                            showNetworkFailureError(viewController: self, statusCode: 500, error: NSError())
                         }
                     } else {
                         self.stopAnimating()
