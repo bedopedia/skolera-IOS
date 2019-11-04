4b929019196623c173d33e6c9ecec3a7313b3982
Rana Hossam <ranahossam263@gmail.com>
2019-11-04 16:00:45 +0200

    Hide new message from HOD / Admin types

--- skolera/skolera/Quizzes/Controllers/QuizStatusViewController.swift
+++ skolera/skolera/Quizzes/Controllers/QuizStatusViewController.swift
@@ -199,7 +199,7 @@ class QuizStatusViewController: UIViewController, NVActivityIndicatorViewable {
         } else {
             studentFinishedQuizView.isHidden = true
             solveQuizButton.isHidden = false
-            if !getUserType().elementsEqual("student") {
+            if getUserType() != UserType.student {
                 notStartedQuizView.isHidden = false
             } else {
                 notStartedQuizView.isHidden = true
@@ -221,11 +221,11 @@ class QuizStatusViewController: UIViewController, NVActivityIndicatorViewable {
             quizDateLabel.text = "Solved".localized
         } else {
             gradeView.isHidden = true
-            if !getUserType().elementsEqual("student") {
+            if getUserType() != UserType.student {
                 notStartedQuizView.isHidden = false
             }
         }
-        if getUserType().elementsEqual("parent") {
+        if getUserType() == UserType.parent {
             //constraints = 0, isHidden = true
             answersHeightConstraint.constant = 0
             questionsHeightConstraint.constant = 0
