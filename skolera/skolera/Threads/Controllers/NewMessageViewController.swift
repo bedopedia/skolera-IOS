bb0167dd4871593d4a037feb592b4729140e1783
Rana Hossam <ranahossam263@gmail.com>
2019-11-06 14:29:42 +0200

    Cherrypick#2

--- skolera/skolera/Threads/Controllers/NewMessageViewController.swift
+++ skolera/skolera/Threads/Controllers/NewMessageViewController.swift
@@ -57,36 +57,24 @@ class NewMessageViewController: UIViewController, UITableViewDelegate, UITableVi
     func getSubjects() {
         startAnimating(CGSize(width: 150, height: 150), message: "", type: .ballScaleMultiple, color: getMainColor(), backgroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).withAlphaComponent(0.5), fadeInAnimation: nil)
         let parameters : Parameters = ["source" : "home"]
-        let headers : HTTPHeaders? = getHeaders()
-        debugPrint(userId())
-        let url = String(format: GET_THREADS_COURSE_GROUPS(),child.actableId)
-        Alamofire.request(url, method: .get, parameters: parameters, headers: headers).validate().responseJSON { response in
+        getSubjectsApi(parameters: parameters, child: child) { (isSuccess, statusCode, response, error) in
             self.stopAnimating()
-            switch response.result{
-            case .success(_):
-                if let result = response.result.value as? [[String : AnyObject]] {
-                    for subject in result {
+            if isSuccess {
+               if let result = response as? [[String : AnyObject]] {
+                    for subject in result
+                    {
                         debugPrint("\(subject)\n")
                         self.subjects.append(Subject.init(fromDictionary: subject))
                     }
                     self.resultTableView.reloadData()
                     self.resultTableView.isHidden = false
                 }
-            case .failure(let error):
-                print(error.localizedDescription)
-                if let err = error as? URLError, err.code  == URLError.Code.notConnectedToInternet {
-                    showAlert(viewController: self, title: ERROR, message: NO_INTERNET, completion: {action in
-                        })
-                } else if response.response?.statusCode == 401 || response.response?.statusCode == 500 {
-                    showReauthenticateAlert(viewController: self)
-                } else {
-                    showAlert(viewController: self, title: ERROR, message: SOMETHING_WRONG, completion: {action in
-                        })
-                }
+            } else {
+                showNetworkFailureError(viewController: self, statusCode: statusCode, error: error!)
             }
         }
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if showTeachers {
             return self.subjects[selectedCoursePos].teachers.count
