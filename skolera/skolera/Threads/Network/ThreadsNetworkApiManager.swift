97e264b50612dd992b11ebcd074de5ac5699b2b0
Rana Hossam <ranahossam263@gmail.com>
2019-11-06 14:46:24 +0200

    Cherrypick#6

--- skolera/skolera/Threads/Network/ThreadsNetworkApiManager.swift
+++ skolera/skolera/Threads/Network/ThreadsNetworkApiManager.swift
@@ -51,3 +51,28 @@ func setThreadSeenApi(parameters: Parameters,completion: @escaping ((Bool, Int, 
     }
 }
 
+
+ func uploadImageApi(threadId: Int, imageData: Data, imageName: String, completion: @escaping ((Bool, Int, Error?) -> ())) {
+    let headers : HTTPHeaders? = getHeaders()
+    let url = "\(String(format: SEND_MESSAGE(),threadId))/messages"
+    Alamofire.upload(
+        multipartFormData: { multipartFormData in
+            multipartFormData.append("file attached".data(using: .utf8)!, withName: "body")
+            multipartFormData.append("\(imageName)".data(using: .utf8)!, withName: "filename")
+            multipartFormData.append(imageData, withName: "attachment", fileName: imageName, mimeType: "image/jpeg")
+    },
+        to: url, method: .post, headers: headers,
+        encodingCompletion: { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    let status = response.response!.statusCode
+                    completion(true, status, nil)
+                }
+            case .failure(let encodingError):
+                print(encodingError)
+                completion(false, -1, encodingError)
+            }
+    })
+    
+}
