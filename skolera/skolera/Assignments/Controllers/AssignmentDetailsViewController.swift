fb8e4645e8e163fe55e5c92bb530e7cc3c856f65
Rana Hossam <ranahossam263@gmail.com>
2019-11-14 15:13:16 +0200

    Edit assignment details

--- skolera/skolera/Assignments/Controllers/AssignmentDetailsViewController.swift
+++ skolera/skolera/Assignments/Controllers/AssignmentDetailsViewController.swift
@@ -14,6 +14,7 @@ class AssignmentDetailsViewController: UIViewController {
     @IBOutlet weak var childImageView: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet var headerView: UIView!
     
     var child : Child!
     var assignment: FullAssignment!
@@ -21,6 +22,7 @@ class AssignmentDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerView.addShadow()
         backButton.setImage(backButton.image(for: .normal)?.flipIfNeeded(), for: .normal)
         if let child = child{
             childImageView.childImageView(url: child.avatarUrl, placeholder: "\(child.firstname.first!)\(child.lastname.first!)", textSize: 14)
