58c768ade45bfc6235c8e083cdc1ebf9a0b31fd6
Rana Hossam <ranahossam263@gmail.com>
2019-12-17 11:37:00 +0200

    edit main color

--- skolera/skolera/Helpers/App Methods.swift
+++ skolera/skolera/Helpers/App Methods.swift
@@ -116,15 +116,21 @@ func image( _ image:UIImage, withSize newSize:CGSize) -> UIImage {
 }
 
 func getMainColor() -> UIColor {
-    if getUserType() == UserType.student {
-        return #colorLiteral(red: 0.9921568627, green: 0.5098039216, blue: 0.4078431373, alpha: 1)
-    } else {
-        if getUserType() == UserType.parent {
-            return #colorLiteral(red: 0.02352941176, green: 0.768627451, blue: 0.8, alpha: 1)
+    let userDefault =  UserDefaults.standard
+    if userDefault.string(forKey: ID) != nil {
+        if getUserType() == UserType.student {
+            return #colorLiteral(red: 0.9921568627, green: 0.5098039216, blue: 0.4078431373, alpha: 1)
         } else {
-            return #colorLiteral(red: 0, green: 0.4941176471, blue: 0.8980392157, alpha: 1)
+            if getUserType() == UserType.parent {
+                return #colorLiteral(red: 0.02352941176, green: 0.768627451, blue: 0.8, alpha: 1)
+            } else {
+                return #colorLiteral(red: 0, green: 0.4941176471, blue: 0.8980392157, alpha: 1)
+            }
         }
+    } else {
+        return #colorLiteral(red: 0.1561536491, green: 0.7316914201, blue: 0.3043381572, alpha: 1)
     }
+    
 }
 
 func clearUserDefaults() {
