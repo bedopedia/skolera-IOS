0008c9e914639e40b34d3ab62626bcc487649aba
Rana Hossam <ranahossam263@gmail.com>
2020-01-06 18:36:45 +0200

    create dummy grades

--- skolera/skolera/Grades/Model/DummyGradebook.swift
+++ skolera/skolera/Grades/Model/DummyGradebook.swift
@@ -1,231 +1,232 @@
-////
-////  DummyGradebook.swift
-////  skolera
-////
-////  Created by Rana Hossam on 1/6/20.
-////  Copyright © 2020 Skolera. All rights reserved.
-////
 //
-//import Foundation
+//  DummyGradebook.swift
+//  skolera
 //
-//public getDummyGradeBook() -> [String: Any]  {
-//return "[
-//"id": 1,
-//"name": "Term 1",
-//"start_date": "2019-09-10T00:00:00.000Z",
-//"end_date": "2020-01-15T23:59:59.000Z",
-//"created_at": "2019-11-11T10:51:43.000Z",
-//"updated_at": "2019-11-11T10:51:43.000Z",
-//"level_id": null,
-//"academic_term_id": 2,
-//"deleted_at": null,
-//"weight": 50.0,
-//"parent_id": null,
-//"lock": false,
-//"publish": false,
-//"category_is_numeric": false,
-//"courses_grading_period_id": 99,
-//"categories": [
-//[
-//"id": 2,
-//"weight": 100.0,
-//"name": "Ungraded",
-//"parent_id": null,
-//"total": 72.0,
-//"grade": "****",
-//"quizzes_total": 6.0,
-//"quizzes_grade": 0.0,
-//"quizzes": [
-//[
-//"id": 5,
-//"name": "quiz 1",
-//"status": null,
-//"category_id": 2,
-//"type": "quiz",
-//"total": 1.0,
-//"grade": 0.0,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 6,
-//"name": "QUIZ 2",
-//"status": null,
-//"category_id": 2,
-//"type": "quiz",
-//"total": 5.0,
-//"grade": 0.0,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//]
-//],
-//"assignments_total": 46,
-//"assignments_grade": 0.0,
-//"assignments": [
-//[
-//"id": 51,
-//"name": "The polygons - Measure the length",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 4,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 60,
-//"name": "The polygons ",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 4,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 70,
-//"name": "Measure the length",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 4,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 79,
-//"name": "The solids",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 4,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 93,
-//"name": "Solids",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 99,
-//"name": "Solids",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 104,
-//"name": "Measuring the mass",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 123,
-//"name": "o'clock",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 126,
-//"name": "Clock",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//],
-//[
-//"id": 134,
-//"name": "Final Revision Sheet",
-//"category_id": 2,
-//"type": "assignment",
-//"total": 5,
-//"grade": 0.0,
-//"status": null,
-//"hide_grade": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": "N/A"
-//]
-//],
-//"grade_items_total": 20,
-//"grade_items_grade": 19.0,
-//"grade_items": [
-//[
-//"id": 10,
-//"name": "November Quiz",
-//"category_id": 2,
-//"hide_grade": false,
-//"type": "grade_item",
-//"total": 20,
-//"grade": 19.0,
-//"status": 0,
-//"feedback_content": null,
-//"feedback_id": null,
-//"grade_view": 19.0
-//]
-//],
-//"percentage": 26.39,
-//"grade_view": "****",
-//"sub_categories": []
-//]
-//],
-//"total": 100.0,
-//"grade": "****",
-//"percentage": 13.2,
-//"grade_view": "****",
-//"letter_scale": "****",
-//"gpa_scale": "****"
-//]
-//]"
-//}
+//  Created by Rana Hossam on 1/6/20.
+//  Copyright © 2020 Skolera. All rights reserved.
 //
+
+import Foundation
+
+public func getDummyGradeBook() -> [String: Any] {
+    /////
+return [
+"id": 1,
+"name": "Term 1",
+"start_date": "2019-09-10T00:00:00.000Z",
+"end_date": "2020-01-15T23:59:59.000Z",
+"created_at": "2019-11-11T10:51:43.000Z",
+"updated_at": "2019-11-11T10:51:43.000Z",
+"level_id": nil,
+"academic_term_id": 2,
+"deleted_at": nil,
+"weight": 50.0,
+"parent_id": nil,
+"lock": false,
+"publish": false,
+"category_is_numeric": false,
+"courses_grading_period_id": 99,
+"categories": [
+[
+"id": 2,
+"weight": 100.0,
+"name": "Ungraded",
+"parent_id": nil,
+"total": 72.0,
+"grade": "****",
+"quizzes_total": 6.0,
+"quizzes_grade": 0.0,
+"quizzes": [
+[
+"id": 5,
+"name": "quiz 1",
+"status": nil,
+"category_id": 2,
+"type": "quiz",
+"total": 1.0,
+"grade": 0.0,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 6,
+"name": "QUIZ 2",
+"status": nil,
+"category_id": 2,
+"type": "quiz",
+"total": 5.0,
+"grade": 0.0,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+]
+],
+"assignments_total": 46,
+"assignments_grade": 0.0,
+"assignments": [
+[
+"id": 51,
+"name": "The polygons - Measure the length",
+"category_id": 2,
+"type": "assignment",
+"total": 4,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 60,
+"name": "The polygons ",
+"category_id": 2,
+"type": "assignment",
+"total": 4,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 70,
+"name": "Measure the length",
+"category_id": 2,
+"type": "assignment",
+"total": 4,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 79,
+"name": "The solids",
+"category_id": 2,
+"type": "assignment",
+"total": 4,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 93,
+"name": "Solids",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 99,
+"name": "Solids",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 104,
+"name": "Measuring the mass",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 123,
+"name": "o'clock",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 126,
+"name": "Clock",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+],
+[
+"id": 134,
+"name": "Final Revision Sheet",
+"category_id": 2,
+"type": "assignment",
+"total": 5,
+"grade": 0.0,
+"status": nil,
+"hide_grade": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": "N/A"
+]
+],
+"grade_items_total": 20,
+"grade_items_grade": 19.0,
+"grade_items": [
+[
+"id": 10,
+"name": "November Quiz",
+"category_id": 2,
+"hide_grade": false,
+"type": "grade_item",
+"total": 20,
+"grade": 19.0,
+"status": 0,
+"feedback_content": nil,
+"feedback_id": nil,
+"grade_view": 19.0
+]
+],
+"percentage": 26.39,
+"grade_view": "****",
+"sub_categories": []
+]
+],
+"total": 100.0,
+"grade": "****",
+"percentage": 13.2,
+"grade_view": "****",
+"letter_scale": "****",
+"gpa_scale": "****"
+]
+//////
+}
+
