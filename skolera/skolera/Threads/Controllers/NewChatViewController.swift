a7814fac2d6de1e63600c75799cee557bdd1f66d
Salma Khalil <salma.medhat@trianglz.com>
2020-06-29 17:41:26 +0200

    add new controller view

---
+++ skolera/skolera/Threads/Controllers/NewChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewChatViewController.swift
+//  skolera
+//
+//  Created by Salma Medhat on 6/29/20.
+//  Copyright © 2020 Skolera. All rights reserved.
+//
+
+import UIKit
+
+class NewChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
