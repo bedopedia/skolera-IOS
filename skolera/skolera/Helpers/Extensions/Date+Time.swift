004f327f7ddbcb7fdeb76e88b2585a6774471c6c
Salma Khalil <salma.medhat@trianglz.com>
2020-07-15 12:11:35 +0200

    timetable now set today and tomorrow due to server zone

--- skolera/skolera/Helpers/Extensions/Date+Time.swift
+++ skolera/skolera/Helpers/Extensions/Date+Time.swift
@@ -15,5 +15,14 @@ extension Date {
     func isBetween(_ date1: Date, and date2: Date) -> Bool {
         return (min(date1, date2) ... max(date1, date2)).contains(self)
     }
+    
+        func convertTimeZone(from initTimeZone: TimeZone, to targetTimeZone: TimeZone) -> Date {
+            let delta = TimeInterval(initTimeZone.secondsFromGMT() - targetTimeZone.secondsFromGMT())
+            debugPrint("DATEEEE:", addingTimeInterval(delta))
+            return addingTimeInterval(delta)
+        }
+    
+    
+    
 }
 
